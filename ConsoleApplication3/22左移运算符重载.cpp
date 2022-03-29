ee8b5810fc0a5e3a8624842e80a7ac7d22429d9b
SOFdian <1462091643@qq.com>
2022-03-29 19:54:21 +0800

    normal

---
+++ ConsoleApplication3/22左移运算符重载.cpp
@@ -0,0 +1,16 @@
+//#include<iostream>
+//using namespace std;
+//
+//class Person {
+//	friend ostream& operator<<(ostream& cout, Person& p);
+//	int a=7;
+//};
+//ostream& operator<<(ostream &cout,Person &p) {
+//	cout << p.a ;
+//	return cout;
+//}
+//int main() {
+//	Person p;
+//	cout << p << endl ;
+//	//cout<<p======cout<<p.a
+//}
