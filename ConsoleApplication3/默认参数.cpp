a71fbe2abbe5a8fc406efbfdc406e6bb1d3039d9
SOFdian <1462091643@qq.com>
2022-04-02 08:44:48 +0800

    normal

--- ConsoleApplication3/默认参数.cpp
+++ ConsoleApplication3/默认参数.cpp
@@ -1,10 +1,10 @@
 //#include<iostream>
 //using namespace std;
 //
-//int add(int x, int y, int z=25);
+//int add(int x, int y, int z = 25);
 //
 //int main() {
-//	cout << add(1, 2, 3)<<endl;
+//	cout << add(1, 2, 3) << endl;
 //	cout << add(1, 2);
 //}
 //
